7770b323145a09c8db3db8f34eb3de77ca7ec885
Guilherme Lopes <guilherme@gwm-solutions.com>
2021-11-24 10:05:37 -0800

    AG-6048 - remove IE specific code

--- src/rich-grid-example/HeaderGroupComponent.vue
+++ src/rich-grid-example/HeaderGroupComponent.vue
@@ -56,7 +56,6 @@
     .expanded {
         animation-name: toExpanded;
         animation-duration: 1s;
-        -ms-transform: rotate(180deg); /* IE 9 */
         -webkit-transform: rotate(180deg); /* Chrome, Safari, Opera */
         transform: rotate(180deg);
     }
@@ -65,7 +64,6 @@
         color: cornflowerblue;
         animation-name: toCollapsed;
         animation-duration: 1s;
-        -ms-transform: rotate(0deg); /* IE 9 */
         -webkit-transform: rotate(0deg); /* Chrome, Safari, Opera */
         transform: rotate(0deg);
     }
@@ -73,13 +71,11 @@
     @keyframes toExpanded {
         from {
             color: cornflowerblue;
-            -ms-transform: rotate(0deg); /* IE 9 */
             -webkit-transform: rotate(0deg); /* Chrome, Safari, Opera */
             transform: rotate(0deg);
         }
         to {
             color: black;
-            -ms-transform: rotate(180deg); /* IE 9 */
             -webkit-transform: rotate(180deg); /* Chrome, Safari, Opera */
             transform: rotate(180deg);
         }
@@ -88,13 +84,11 @@
     @keyframes toCollapsed {
         from {
             color: black;
-            -ms-transform: rotate(180deg); /* IE 9 */
             -webkit-transform: rotate(180deg); /* Chrome, Safari, Opera */
             transform: rotate(180deg);
         }
         to {
             color: cornflowerblue;
-            -ms-transform: rotate(0deg); /* IE 9 */
             -webkit-transform: rotate(0deg); /* Chrome, Safari, Opera */
             transform: rotate(0deg);
         }
